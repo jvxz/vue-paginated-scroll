308f4d9903601b8b8bdf56af84286048b5f5179e
Jamie <hi@jamie.to>
2026-07-04 12:57:37 -0400

    chore: add main entry point

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export { usePaginatedScroll } from './usePaginatedScroll'
+export { default as PaginatedScrollDebug } from './PaginatedScrollDebug.vue'
+export {
+  distanceToEdge,
+  isSafeToTrim,
+  resolveTrigger,
+  isScrolledToForwardEnd,
+} from './geometry'
+export type {
+  Direction,
+  Edge,
+  ItemKey,
+  PaginatedScroll,
+  PaginatedScrollOptions,
+  DebugState,
+  TriggerDebugInfo,
+} from './types'
